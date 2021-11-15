49f2f322bb086858eb6582afeb49ff79ebff9580
Asal Shamim <41086746+asalshamim@users.noreply.github.com>
2021-11-15 21:56:45 +0100

    1

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
 /*Här skriver ni koden*/
-const todoBtn=document.getElementById("my-button");
-let todoList=document.getElementById("list");
+const todoBtn =document.getElementById("my-button");
+const todoList =document.getElementById("list");
+
 
 
 todoBtn.addEventListener("click", function() {
   let textinput= document.getElementById("task").value;
 
-
     if (textinput === ""){
       alert ("Text")
     }
     else {
+      
 
       const newTodo= document.createElement("LI"); //  Li
       newTodo.innerText =textinput;
@@ -26,48 +27,53 @@ todoBtn.addEventListener("click", function() {
 
         const list = document.getElementById("list");
        list.removeChild(list.childNodes[0]);
+
+      
+      
        
 
       })
       
 
     }
+  
+    
   })
   
-
+      
  // Save list
 
  function saveList() {
-   let toDos = [];
-   for(let i=0; i<todoList.children.length; i++){
-     let toDo = todoList.children.item(i);
-     
+  let toDos = [];
+  for(let i=0; i<todoList.children.length; i++){
+    let toDo = todoList.children.item(i);
+    
 
-     let todoInfo ={
-       task: todoList.innerText,
-       class: todoList.children[i].className
-     };
-     
-     toDos.push(todoInfo);
-   }
-   localStorage.setItem("toDos", JSON.stringify(toDos));
-  
- }
+    let todoInfo ={
+      task: todoList.innerText,
+      class: todoList.children[i].className
+    };
+    
+    toDos.push(todoInfo);
+  }
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+ 
+}
 
- function loadList() {
-   if(localStorage.getItem("toDos") != null){
-     let toDos = JSON.parse(localStorage.getItem("toDos"));
+function loadList() {
+  if(localStorage.getItem("toDos") != null){
+    let toDos = JSON.parse(localStorage.getItem("toDos"));
 
-     for (let i= 0; i< toDos.length; i++){
-       let toDo = toDos[i];
-       newTodoItem(toDo.task, toDo.class);
-     }
-   }
-   loadList();
+    for (let i= 0; i< toDos.length; i++){
+      let toDo = toDos[i];
+      newTodoItem(toDo.task, toDo.class);
+    }
+  }
+  loadList();
 
 
- }
- function saveCount(){
-   saveList();
-   loadList();
- }
+}
+function saveCount(){
+  saveList();
+  loadList();
+}
